313ad006edb0ac4033340c3b228f442be869c607
zenlee-george <zenlee.george1@gmail.com>
2024-07-05 16:43:34 +0200

    :construction:

--- src/components/aboutComp.vue
+++ src/components/aboutComp.vue
@@ -1,13 +1,20 @@
 <template>
     <div>
-        
+      <h1>About Me</h1>
+      <p v-if="aboutMe">{{ aboutMe }}</p>
+      <p v-else>Loading...</p>
     </div>
-</template>
-<script>
-export default {
-    
-}
-</script>
-<style >
-    
-</style>
+  </template>
+  
+  <script>
+  export default {
+    computed: {
+      aboutMe() {
+        return this.$store.state.aboutMe
+      }
+    },
+    mounted() {
+        this.$store.dispatch('fetchPortfolio')
+    }
+  }
+  </script>
