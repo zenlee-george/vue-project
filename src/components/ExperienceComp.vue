569dcfbf32e0e496d73f9176c8afddc9d181091f
zenlee-george <zenlee.george1@gmail.com>
2024-07-15 17:04:13 +0200

    memo

--- src/components/ExperienceComp.vue
+++ src/components/ExperienceComp.vue
@@ -2,7 +2,7 @@
     <div class="container" id="experience-section">
       <div class="row pt-5">
         <h2 class="exp-heading headings">Experience</h2>
-        <p class="subtext pt-5">It's not about the destination ... it's about the experience.</p>
+        <p class="subtext pt-5">It's not about the reward... it's about the effort</p>
       </div>
       <div class="row">
         <div class="col d-flex justify-content-center">
